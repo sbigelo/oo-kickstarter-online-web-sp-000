6a6a4c81f5123afae6d8d503779a9729f1fd41f9
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-25 19:48:32 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,16 @@
 class Backer
   attr_reader :name, :backed_projects
   
+  
+  
   def initialize(name)
     @name = name
-    @backed_projects = []
+    @@backed_projects = []
   end
   
   def back_project(project)
     @backed_projects << project
-    @backers << project
+ 
   end
   
   
